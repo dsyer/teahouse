a44858ee991d98af55e512703d1092ee4f8a1e30
Dave Syer <dsyer@vmware.com>
2023-10-10 13:31:23 +0100

    Remove unused apps

--- spring-boot-admin/src/test/java/org/example/teahouse/sba/SbaApplicationTest.java
+++
@@ -1,9 +0,0 @@
-package org.example.teahouse.sba;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest(classes = SbaApplication.class)
-class SbaApplicationTest {
-    @Test void contextLoads() {}
-}
